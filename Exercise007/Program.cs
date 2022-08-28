b5428615846c63ab9f9b586eb4a1c0df3eca1e8d
Brandon <brandon.ebarnett@gmail.com>
2022-08-28 02:20:34 -0400

    Add project files.

---
+++ Exercise007/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Write a program that asks the user to enter a number.
+ * Compute the factorial of the number and print it on the console.
+ Example: If the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1
+          and display it as 5! = 120 */
+
+Console.WriteLine("Please enter a number to get its factorial:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial = CalculateFactorial(number);
+Console.WriteLine(number + "! = " + factorial);
+
+int CalculateFactorial(int number)
+{
+    int factorial = 1;
+    for (int i = number; i > 0; i--)
+        factorial *= i;
+    return factorial;
+}
